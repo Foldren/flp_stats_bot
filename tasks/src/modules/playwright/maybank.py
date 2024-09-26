3b2e4adcb007822b2addf5ed83e5f5951d36f009
k.demchenko <k.demchenko@cfunalog.ru>
2024-09-26 15:33:59 +0300

    выписки выводятся корректно осталось сохранить в бд и настроить капчу

--- tasks/src/modules/playwright/maybank.py
+++ tasks/src/modules/playwright/maybank.py
@@ -1,3 +1,4 @@
+import json
 from asyncio import sleep
 from io import BytesIO
 from PIL import Image
@@ -74,7 +75,8 @@ class Maybank:
             payment_accounts = []
             for i in range(1, row_count):
                 payment_accounts.append({
-                    "number": await table.locator('tr').nth(i).locator('td').nth(0).locator("#ui_button_label_").inner_text(),
+                    "number": await table.locator('tr').nth(i).locator('td').nth(0).locator(
+                        "#ui_button_label_").inner_text(),
                     "currency": await table.locator('tr').nth(i).locator('td').nth(1).inner_text()
                 })
 
@@ -88,28 +90,37 @@ class Maybank:
         for page in self.pages:
             frame = page.frame_locator("#frameSet_1_8_content")
             await frame.get_by_text("Transaction Activity").click()
+            await page.wait_for_timeout(3000)
 
         # Сохраняем выписки ----------------------------------------------------------------------------------------
         pas_statements = {}
         i = 0
-        while self.pages:
+        len_pages = len(self.pages)
+        while len_pages != 0:
             # Формируем вырезку расчетных счетов по индексу для каждой страницы пока они не кончатся
             pages_index_pas = []
             for pas in pages_pas:
                 try:
                     pages_index_pas.append(pas[i])
                 except IndexError:
-                    try:
+                    len_pages -= 1
+                    if len(self.pages) > 1:
                         self.pages.pop(i)
-                    except IndexError:
+                    else:
                         break
+
+            print(pages_index_pas)
+            if len_pages == 0:
+                break
+
             i += 1
 
             for payment_account in pages_index_pas:
                 for page in self.pages:
                     frame = page.frame_locator("#frameSet_1_8_content")
                     # todo получить последнюю дату отгрузки
-                    await frame.locator("#searchCriteriaLabel2 .input_value[name='transactionHistoryVO.dateFrom']").evaluate(
+                    await frame.locator(
+                        "#searchCriteriaLabel2 .input_value[name='transactionHistoryVO.dateFrom']").evaluate(
                         "(element, value) => element.value = value", "2024-01-01")
                     await frame.locator("#searchCriteriaLabel1 input[name='corpVO.accountNumber']").evaluate(
                         "(element, value) => element.value = value", payment_account["number"])
@@ -118,50 +129,55 @@ class Maybank:
                     await frame.locator("#searchCriteriaLabel2 #ui_button_label_goButton").click()
 
                 for page in self.pages:
-                    await page.wait_for_timeout(2000)
                     frame = page.frame_locator("#frameSet_1_8_content")
                     have_next_button = True
 
                     while have_next_button:
+                        await page.wait_for_timeout(3000)
                         table = frame.locator(".infotable tbody").nth(1)
-
                         row_count = await table.locator("tr").count()
 
                         if row_count > 1:
-                            for i in range(1, row_count):
-                                columns = table.locator('tr').nth(i).locator('td')
+                            for k in range(1, row_count):
+                                columns = table.locator('tr').nth(k).locator('td')
                                 debit = await columns.nth(6).locator("div").nth(1).inner_text()
                                 credit = await columns.nth(7).locator("div").nth(1).inner_text()
 
-                                print({
-                                        "date": await columns.nth(2).locator("div").nth(1).inner_text(),
-                                        "description": await columns.nth(4).locator("div").nth(1).inner_text(),
-                                        "id": await columns.nth(5).locator("div").nth(1).inner_text(),
-                                        "amount": debit if debit != "-" else credit
-                                })
+                                have_date = await columns.nth(2).locator("div").nth(1).count()
+                                have_descr = await columns.nth(4).locator("div").nth(1).count()
+                                have_id = await columns.nth(5).locator("div").nth(1).count()
+
+                                if have_date:
+                                    date = await columns.nth(2).locator("div").nth(1).inner_text()
+                                else:
+                                    date = None
+                                if have_descr:
+                                    descr = await columns.nth(4).locator("div").nth(1).inner_text()
+                                else:
+                                    descr = None
+                                if have_id:
+                                    trxn_id = await columns.nth(5).locator("div").nth(1).inner_text()
+                                else:
+                                    trxn_id = None
+
+                                transaction = {
+                                    "date": date,
+                                    "description": descr,
+                                    "id": trxn_id,
+                                    "amount": debit if debit != "-" else "-" + credit
+                                }
 
                                 try:
-                                    pas_statements[payment_account["number"]][payment_account["currency"]].append({
-                                        "date": await columns.nth(2).locator("div").nth(1).inner_text(),
-                                        "description": await columns.nth(4).locator("div").nth(1).inner_text(),
-                                        "id": await columns.nth(5).locator("div").nth(1).inner_text(),
-                                        "amount": debit if debit != "-" else credit
-                                })
+                                    pas_statements[payment_account["number"] + " " + payment_account["currency"]].append(transaction)
                                 except KeyError:
-                                    pas_statements[payment_account["number"]][payment_account["currency"]] = [{
-                                        "date": await columns.nth(2).locator("div").nth(1).inner_text(),
-                                        "description": await columns.nth(4).locator("div").nth(1).inner_text(),
-                                        "id": await columns.nth(5).locator("div").nth(1).inner_text(),
-                                        "amount": debit if debit != "-" else credit
-                                }]
-
-                        print(pas_statements)
+                                    pas_statements[payment_account["number"] + " " + payment_account["currency"]] = [transaction]
 
                         have_next_button = await frame.locator("#pageObject_pageIndex_nextBtn").count()
 
                         if have_next_button:
                             await frame.locator("#pageObject_pageIndex_nextBtn").click()
 
+        print(json.dumps(pas_statements, indent=4))
 
     async def load_stats(self) -> None:
         # f = Fernet(SECRET_KEY)
@@ -187,5 +203,7 @@ class Maybank:
 
             # Получаем список транзакций
             await self.__get_transactions(pages_pas=pas)
+
+            print("end")
 
             await sleep(1000000)
